3c74c475332182509e7b3e12236a6e56adc8b5d9
wyicwx <wyicwx@gmail.com>
2014-01-04 15:39:28 +0800

    rebuild

---
+++ configs/config.js
@@ -0,0 +1,27 @@
+var jtconfig = {
+
+	builder: {
+		// develop path
+		base: '../',
+		// folds path for ignore
+		ignorePath: [],
+		//
+		outputPath: '../output'
+	},
+
+	proxy: {
+		slowLoad: false,
+		slowBlockByte: 10240,
+		slowTimeInterval: 100,
+		slient: true,
+		port: {
+			"8080": "default"
+		}
+	},
+	
+	processor: {
+		gzip: true
+	}
+};
+
+module.exports = jtconfig;
