49e872093a302693f2f1aae2e3a93b2ff814e1b8
wyicwx <wyicwx@gmail.com>
2014-02-07 19:13:45 +0800

    add Makefile

---
+++ test/kernel.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+describe('kernel', function() {
+	it('not throw error', function() {
+		try {
+			require('../lib/kernel.js');
+		} catch(e) {
+			assert.ok(false);
+			return;
+		}
+		assert.ok(true);		
+	});	
+});
